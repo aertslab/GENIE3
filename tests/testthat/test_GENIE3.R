fe5a8ad1d37797a0380d55d609980f56f581e57d
u0103631 <u0103631@gbw-s-seq07.luna.kuleuven.be>
2017-01-16 14:33:38 +0100

    Several minor updates: - Namespace not to require the paralell functions - Tests not to expect square matrix - GENIE3.R re-ordered some code, repaired bug with regulators ("out of range"). - Removed folder inst/doc

--- tests/testthat/test_GENIE3.R
+++ tests/testthat/test_GENIE3.R
@@ -1,11 +1,11 @@
-test_that("GENIE3 works properly", {
-	
+test_that("GENIE3 tests", {
+
 	expr.matrix <- matrix(sample(1:10, 100, replace=TRUE), nrow=20)
 	rownames(expr.matrix) <- paste("Gene", 1:20, sep="")
 	colnames(expr.matrix) <- paste("Sample", 1:5, sep="")
-	
+
 	weight.matrix <- GENIE3(expr.matrix)
-	
+
 	expect_equal(dim(weight.matrix)[1], 20)
 	expect_equal(dim(weight.matrix)[2], 20)
 	expect_true(is.numeric(weight.matrix))
@@ -14,33 +14,33 @@ test_that("GENIE3 works properly", {
 	expect_equal(sum(diag(weight.matrix)), 0)
 	expect_equal(sum(rownames(weight.matrix) == colnames(weight.matrix)), 20)
 	expect_equal(sum(rownames(weight.matrix) == rownames(expr.matrix)), 20)
-	
-	
+
+
 	regulators <- c(2,4,6)
 	weight.matrix <- GENIE3(expr.matrix, tree.method="ET", K="all", ntrees=100, regulators=regulators)
 	zidx <- setdiff(1:20, regulators)
-	expect_equal(sum(weight.matrix[zidx,]), 0)
+	# expect_equal(sum(weight.matrix[zidx,]), 0)
 	expect_equal(sum(weight.matrix < 0), 0)
-	expect_equal(dim(weight.matrix)[1], 20)
-	expect_equal(dim(weight.matrix)[2], 20)
+	expect_equal(nrow(weight.matrix), 3)
+	expect_equal(ncol(weight.matrix), 20)
 	expect_true(is.numeric(weight.matrix))
 	expect_equal(sum(is.na(weight.matrix)), 0)
-	expect_equal(sum(diag(weight.matrix)), 0)
-	expect_equal(sum(rownames(weight.matrix) == colnames(weight.matrix)), 20)
-	expect_equal(sum(rownames(weight.matrix) == rownames(expr.matrix)), 20)
-	
-	
-	regulators <- c("Gene5","Gene9")
+	expect_equal(sum(diag(weight.matrix[,rownames(weight.matrix)])), 0)
+	# expect_equal(sum(rownames(weight.matrix) == colnames(weight.matrix)), 20)
+	expect_equal(sum(colnames(weight.matrix) == rownames(expr.matrix)), 20)
+
+
+	regulators <- rownames(expr.matrix)[c(5,9)]
 	weight.matrix <- GENIE3(expr.matrix, tree.method="ET", K=1, ntrees=100, regulators=regulators)
-	zidx <- setdiff(rownames(weight.matrix), regulators)
-	expect_equal(sum(weight.matrix[zidx,]), 0)
+	zidx <- setdiff(colnames(weight.matrix), regulators)
+	# expect_equal(sum(weight.matrix[zidx,]), 0)
 	expect_equal(sum(weight.matrix < 0), 0)
-	expect_equal(dim(weight.matrix)[1], 20)
-	expect_equal(dim(weight.matrix)[2], 20)
+	expect_equal(nrow(weight.matrix), 2)
+	expect_equal(ncol(weight.matrix), 20)
 	expect_true(is.numeric(weight.matrix))
 	expect_equal(sum(is.na(weight.matrix)), 0)
-	expect_equal(sum(diag(weight.matrix)), 0)
-	expect_equal(sum(rownames(weight.matrix) == colnames(weight.matrix)), 20)
-	expect_equal(sum(rownames(weight.matrix) == rownames(expr.matrix)), 20)
+	expect_equal(sum(diag(weight.matrix[,rownames(weight.matrix)])), 0)
+	# expect_equal(sum(rownames(weight.matrix) == colnames(weight.matrix)), 20)
+	expect_equal(sum(colnames(weight.matrix) == rownames(expr.matrix)), 20)
 })
 
